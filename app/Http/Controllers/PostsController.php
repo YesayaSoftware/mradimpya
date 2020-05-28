0398352cbdefbc70d92b2621b7e850750eddfae4
Yesaya R. Athuman <yesayaathuman@gmail.com>
2020-05-28 04:11:59 +0300

    Fahamu Laravel Eloquent

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use \DB;
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
     public function show($slug) 
     {
-        $post = \DB::table('posts')->where('slug', $slug)->first();
+        $post = Post::where('slug', $slug)->firstOrFail();
 
-        return view('post', [
-            'post' => $post
-        ]);
+        return view('post', ['post' => $post]);
     }
 }
